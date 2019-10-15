486c011de09e983c016cab46b7cb59a6eeba94f0
Charioteer <49540020+nv343@users.noreply.github.com>
2019-10-15 09:57:45 -0300

    0.2
    Created a couple of screens

--- water/src/main/java/so/water/App.java
+++ water/src/main/java/so/water/App.java
@@ -13,26 +13,29 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
+	private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
-        stage.setScene(scene);
-        stage.show();
-    }
+	@Override
+	public void start(Stage stage) throws IOException, InterruptedException {
+		// Conn.getEntityManager().close();
+		scene = new Scene(loadFXML("loading"));
+		stage.setScene(scene);
+		stage.show();
+	}
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+	static void setRoot(String fxml) throws IOException {
+		scene.setRoot(loadFXML(fxml));
+	}
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+	private static Parent loadFXML(String fxml) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+		return fxmlLoader.load();
+	}
 
-    public static void main(String[] args) {
-        launch();
-    }
+	@Override
+	public void stop() throws Exception {
+		// Conn.closeConn();
+		super.stop();
+	}
 
 }
--- water/src/main/java/so/water/PrimaryController.java
+++ water/src/main/java/so/water/PrimaryController.java
@@ -4,9 +4,18 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
+	@FXML
+	private void load() throws IOException {
+		App.setRoot("primary");
+	}
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+	@FXML
+	private void switchToSecondary() throws IOException {
+		App.setRoot("test");
+	}
+	
+	@FXML
+	private void switchToInfo() throws IOException {
+		App.setRoot("info");
+	}
 }
